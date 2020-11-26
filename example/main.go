a170402570abf3391362774ed675085e50b1812b
zenghouchao <greatjerry@foxmail.com>
2020-11-26 14:44:20 +0800

    fix

--- example/main.go
+++ example/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"fmt"
 	env "github.com/zenghouchao/go_env"
+	"os"
+	"runtime"
 )
 
+var osType = runtime.GOOS
+
 func main() {
+	dir, _ := os.Getwd()
+	if osType == "windows" {
+		dir = dir + "\\.env"
+	} else if osType == "linux" {
+		dir = dir + "/.env"
+	}
 	var params env.EnvParams
-	params.ParseEnvFile(".env")
+	params.ParseEnvFile(dir)
 	database := params.GetSection("DATABASE", "DATABASE")
 	fmt.Println(database)
 
