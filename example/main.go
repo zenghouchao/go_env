d0ec430ee89d325cacd267d460ca86dedd34270c
zenghouchao <greatjerry@foxmail.com>
2020-11-26 14:19:51 +0800

    example

--- example/main.go
+++ example/main.go
@@ -1,1 +1,14 @@
-package example
+package main
+
+import (
+	"fmt"
+	env "github.com/zenghouchao/go_env"
+)
+
+func main() {
+	var params env.EnvParams
+	params.ParseEnvFile(".env")
+	database := params.GetSection("DATABASE", "DATABASE")
+	fmt.Println(database)
+
+}
